233414d47644b9e1d22b6fff1fd5c56ca69cbf97
Hong Shick Pak <hong@hspak.com>
2022-07-06 19:57:19 -0700

    Drop max log line to 5000
    It still falls to a crawl with massive log spam.

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func setupProcesses(cfgPath string, tui *tview.Application) []*Process {
 	for _, cfg := range cfgs {
 		processLogsPane := tview.NewTextView().
 			SetScrollable(true).
-			SetMaxLines(10000).
+			SetMaxLines(5000).
 			SetDynamicColors(true).
 			SetChangedFunc(func() {
 				tui.Draw()
